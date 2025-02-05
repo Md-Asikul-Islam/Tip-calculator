ed57883f6dd88140e2aeed3db5228c3909929aa4
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-02-05 10:11:17 +0600

    get all html document

---
+++ script.js
@@ -0,0 +1,6 @@
+const generateBillButton = document.querySelector(".generate-bill");
+const billAmount = document.querySelector(".bill-amount");
+const discountPercentage  = document/queueMicrotask(".discount-percentage");
+const tipPercentage  = document/queueMicrotask(".tip-percentage");
+const noOfCustomers = document.querySelector(".no-of-customers")
+
