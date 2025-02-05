8f73a4ae0ad179a3e085b03fb082ea267739fa16
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-02-05 10:14:47 +0600

    get the tip document element by dom

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const discountPercentage  = document/queueMicrotask(".discount-percentage");
 const tipPercentage  = document/queueMicrotask(".tip-percentage");
 const noOfCustomers = document.querySelector(".no-of-customers")
 
+const totalTipPaid = document.querySelector(".total-tip-paid");
+const totalAmountToPay = document.querySelector(".total-amount-to-pay");
+const totalTipPercentageValue = document.querySelector(".tip-value")
+const discountPercentageValue = document.querySelector(".discount-value")
+const totalNoOfCustomersValue = document.querySelector(".no-of-customers-value")
+const eachCustomerToPay = document.querySelector('.each-customer-to-pay');
+
+
