bb4fc0390a1899d21f648d14459e974414ff2aef
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-02-05 11:18:00 +0600

    finally is work

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 const generateBillButton = document.querySelector(".generate-bill");
 const billAmount = document.querySelector(".bill-amount");
-const discountPercentage  = document/queueMicrotask(".discount-percentage");
-const tipPercentage  = document/queueMicrotask(".tip-percentage");
-const noOfCustomers = document.querySelector(".no-of-customers")
+const discountPercentage = document.querySelector(".discount-percentage");
+const tipPercentage = document.querySelector(".tip-percentage");
+const noOfCustomers = document.querySelector(".no-of-customers");
 
 const totalTipPaid = document.querySelector(".total-tip-paid");
 const totalAmountToPay = document.querySelector(".total-amount-to-pay");
-const totalTipPercentageValue = document.querySelector(".tip-value")
-const discountPercentageValue = document.querySelector(".discount-value")
-const totalNoOfCustomersValue = document.querySelector(".no-of-customers-value")
-const eachCustomerToPay = document.querySelector('.each-customer-to-pay');
-
+const totalTipPercentageValue = document.querySelector(".tip-value");
+const discountPercentageValue = document.querySelector(".discount-value");
+const totalNoOfCustomersValue = document.querySelector(".no-of-customers-value");
+const eachCustomerToPay = document.querySelector(".each-customer-to-pay");
 
 function calculateBill() {
-    console.log(
-      billAmount.value,
-      tipsPercentage.value,
-      discountPercentage.value,
-      noOfCustomers.value
-    );
+  console.log(
+    billAmount.value,
+    tipPercentage.value,
+    discountPercentage.value,
+    noOfCustomers.value
+  );
 
-    const billAmountAfterDiscountPercentage =
+  const billAmountAfterDiscount =
     billAmount.value - (discountPercentage.value * billAmount.value) / 100;
 
-  const getTipAmount =
-    billAmountAfterDiscountPercentage * (tipsPercentage.value / 100);
-  const totalBill = billAmountAfterDiscountPercentage + getTipAmount;
+  const getTipAmount = (billAmountAfterDiscount * tipPercentage.value) / 100;
+  const totalBill = billAmountAfterDiscount + getTipAmount;
   const eachCustomerToPayAmount = totalBill / noOfCustomers.value;
 
+  totalTipPaid.textContent = getTipAmount.toFixed(2);
+  totalAmountToPay.textContent = totalBill.toFixed(2);
+  totalTipPercentageValue.textContent = tipPercentage.value;
+  discountPercentageValue.textContent = discountPercentage.value;
+  totalNoOfCustomersValue.textContent = noOfCustomers.value;
+  eachCustomerToPay.textContent = eachCustomerToPayAmount.toFixed(2);
 }
+
+generateBillButton.addEventListener("click", calculateBill);
